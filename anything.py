0e9b50df369530a3c0cc8804a1226c519c5ac330
Dragonoque67 <sirangusthefirst@gmail.com>
2025-02-20 15:29:30 +0530

    Update anything.py

--- anything.py
+++ anything.py
@@ -4,3 +4,5 @@ st.write("hey")
 st.sidebar.write("sidebar")
 if st.button("coffee"):
   st.write("Let's make coffee")
+  milk=st.number_input("percentage of milk :",value=60)
+  st.info(f"milk amount is {milk}")
