972013e022f02c6d989605d01ff814e823062895
Dragonoque67 <sirangusthefirst@gmail.com>
2025-02-20 15:36:53 +0530

    Update anything.py

--- anything.py
+++ anything.py
@@ -5,5 +5,6 @@ st.sidebar.write("sidebar")
 options= st.selectbox("options",["coffee","juice","tea"])
 if options=="coffee":
   st.write("Let's make coffee")
-  milk=st.number_input("percentage of milk :",value=60)
+  milk=st.number_input("percentage of milk :",value=60,min_value=0,max_value=100,step=5)
   st.info(f"milk amount is {milk}")
+elif options
