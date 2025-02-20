990128818d09f8f602a957b185cf3fbb90d5c7b5
Dragonoque67 <sirangusthefirst@gmail.com>
2025-02-20 15:33:44 +0530

    Update anything.py

--- anything.py
+++ anything.py
@@ -2,7 +2,7 @@
 import streamlit as st
 st.write("hey")
 st.sidebar.write("sidebar")
-options= st.selectbox(["coffee","juice","tea"]):
+options= st.selectbox(["coffee","juice","tea"])
 if options=="coffee":
   st.write("Let's make coffee")
   milk=st.number_input("percentage of milk :",value=60)
