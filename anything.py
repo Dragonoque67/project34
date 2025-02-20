d1744a09aa5720b359b048d5c18772a29386410b
Dragonoque67 <sirangusthefirst@gmail.com>
2025-02-20 15:37:28 +0530

    Update anything.py

--- anything.py
+++ anything.py
@@ -7,4 +7,4 @@ if options=="coffee":
   st.write("Let's make coffee")
   milk=st.number_input("percentage of milk :",value=60,min_value=0,max_value=100,step=5)
   st.info(f"milk amount is {milk}")
-elif options
+
