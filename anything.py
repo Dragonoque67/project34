72264ee338980d9353ec16110660e6aef06e0f4a
Dragonoque67 <sirangusthefirst@gmail.com>
2025-02-21 12:45:27 +0530

    Update anything.py

--- anything.py
+++ anything.py
@@ -1,7 +1,7 @@
 #streamlit app
 import streamlit as st
 st.write("hey little gayboi")
-st.image("WhatsApp Image 2025-02-21 at 12.20.15_c4204c63", caption="me fr")
+
 st.button('fruity')
 st.checkbox('are you gay?')
 st.slider("how much u gay?",0,100)
