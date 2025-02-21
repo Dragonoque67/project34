d6e87ca5c9ccd21748247a19cc9308dcf87f8c11
Dragonoque67 <sirangusthefirst@gmail.com>
2025-02-21 12:54:34 +0530

    Update anything.py

--- anything.py
+++ anything.py
@@ -1,7 +1,8 @@
 #streamlit app
 import streamlit as st
 st.write("hey little gayboi")
-
+image_local = Image.open("yoyo")
+st.image(image_local, caption="Local Image", use_column_width=True)
 st.button('fruity')
 st.checkbox('are you gay?')
 st.slider("how much u gay?",0,100)
