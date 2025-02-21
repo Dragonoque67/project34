e18f9c71f91a3d4b6294c4842190764291358825
Dragonoque67 <sirangusthefirst@gmail.com>
2025-02-21 12:42:15 +0530

    Update anything.py

--- anything.py
+++ anything.py
@@ -1,12 +1,9 @@
 #streamlit app
 import streamlit as st
-st.write("hey customer!")
-st.sidebar.write("sidebar")
-options= st.selectbox("options",["coffee","juice","tea"])
-if options=="coffee":
-  st.write("Let's make coffee")
-  milk=st.number_input("percentage of milk :",value=60,min_value=0,max_value=100,step=5)
-  st.info(f"milk amount is {milk}")
+st.write("hey little gayboi")
+
 st.button('fruity')
 st.checkbox('are you gay?')
 st.slider("how much u gay?",0,100)
+if st.button("garv's gay pics"):
+    st.write("go away rajat")
