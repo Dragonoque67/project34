8fd0f84b3076db18d7a1d42d9edfe7f709f8b310
Dragonoque67 <sirangusthefirst@gmail.com>
2025-02-21 12:15:05 +0530

    Update anything.py

--- anything.py
+++ anything.py
@@ -7,4 +7,6 @@ if options=="coffee":
   st.write("Let's make coffee")
   milk=st.number_input("percentage of milk :",value=60,min_value=0,max_value=100,step=5)
   st.info(f"milk amount is {milk}")
-
+st.button('fruity')
+st.checkbox('are you gay?')
+st.download-button("when did you turn gay?", okay)
